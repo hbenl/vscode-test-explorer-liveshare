176a85e78a7524b5e1d85f7329d048e12ac76cbd
Holger Benl <hbenl@evandor.de>
2018-08-19 13:51:30 +0200

    show an error message if the setting necessary for this extension is missing

--- src/main.ts
+++ src/main.ts
@@ -16,10 +16,10 @@ async function activateAsync(context: vscode.ExtensionContext): Promise<void> {
 	const log = new Log('Test Explorer Live Share');
 
 	log.info('Looking for Test Explorer');
-	const testExplorerExt = vscode.extensions.getExtension<TestHub>(testExplorerExtensionId);
-	const testExplorer = testExplorerExt ? testExplorerExt.exports : undefined;
-	log.info(`Test Explorer ${testExplorer ? '' : 'not '}found`);
-	if (!testExplorer) return;
+	const testExplorerExtension = vscode.extensions.getExtension<TestHub>(testExplorerExtensionId);
+	const testHub = testExplorerExtension ? testExplorerExtension.exports : undefined;
+	log.info(`Test Hub ${testHub ? '' : 'not '}found`);
+	if (!testHub) return;
 
 	log.info('Looking for VSLS');
 	const liveShare = await vsls.getApiAsync();
@@ -29,15 +29,21 @@ async function activateAsync(context: vscode.ExtensionContext): Promise<void> {
 	log.info('Trying to create shared service');
 	const sharedService = await liveShare.shareService(serviceName);
 	log.info(`Shared service ${sharedService ? '' : 'not '}created`);
-	if (!sharedService) return;
+	if (!sharedService) {
+		vscode.window.showErrorMessage('Could not create a shared service. You have to set "liveshare.features" to "experimental" in your user settings in order to use this extension.');
+		return;
+	}
 
 	log.info('Trying to get shared service proxy');
 	const sharedServiceProxy = await liveShare.getSharedService(serviceName);
 	log.info(`Shared service proxy ${sharedServiceProxy ? '' : 'not '}found`);
-	if (!sharedServiceProxy) return;
+	if (!sharedServiceProxy) {
+		vscode.window.showErrorMessage('Could not access a shared service. You have to set "liveshare.features" to "experimental" in your user settings in order to use this extension.');
+		return;
+	}
 
-	new HostSessionManager(context, testExplorer, liveShare, sharedService, log);
-	new GuestSessionManager(context, testExplorer, sharedServiceProxy, log);
+	new HostSessionManager(context, testHub, liveShare, sharedService, log);
+	new GuestSessionManager(context, testHub, sharedServiceProxy, log);
 
 	context.subscriptions.push({ dispose() { liveShare.unshareService(serviceName); } });
 }
